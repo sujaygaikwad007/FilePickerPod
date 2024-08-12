cd00aaf420255a45e585f7dbad6f481c2ffd53e2
Aniket Patil <aniket.p@reapmind.com>
2024-08-12 17:59:07 +0530

    Add Framework

---
+++ FilePickerPod/FilePicker.swift
@@ -0,0 +1,110 @@
+import UIKit
+import MobileCoreServices
+import UniformTypeIdentifiers
+
+public protocol FilePickerDelegate: AnyObject {
+    func didPickImage(_ image: UIImage, url: URL?)
+    func didPickDocument(_ url: URL)
+    func didCancelDocumentPicker()
+    func didSelectInvalidDocument()
+}
+
+public class FilePicker: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIDocumentPickerDelegate {
+
+    private weak var viewController: UIViewController?
+    private weak var delegate: FilePickerDelegate?
+    private var allowedDocumentTypes: Set<String> = []
+
+    public init(viewController: UIViewController, delegate: FilePickerDelegate) {
+        self.viewController = viewController
+        self.delegate = delegate
+    }
+
+    public func pickImage() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.mediaTypes = [UTType.image.identifier]
+        viewController?.present(imagePicker, animated: true, completion: nil)
+    }
+
+    public func pickDocument(type: DocumentType) {
+        allowedDocumentTypes = Set(type.fileTypes)
+        let documentPicker = UIDocumentPickerViewController(documentTypes: type.fileTypes, in: .import)
+        documentPicker.delegate = self
+        viewController?.present(documentPicker, animated: true, completion: nil)
+    }
+
+    // MARK: - UIImagePickerControllerDelegate
+
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            // Save the image to a temporary file and get the URL
+            if let imageData = image.pngData() {
+                let tempDirectory = FileManager.default.temporaryDirectory
+                let tempFileURL = tempDirectory.appendingPathComponent(UUID().uuidString).appendingPathExtension("png")
+                do {
+                    try imageData.write(to: tempFileURL)
+                    delegate?.didPickImage(image, url: tempFileURL)
+                } catch {
+                    print("Error saving image to temporary file: \(error)")
+                    delegate?.didPickImage(image, url: nil)
+                }
+            }
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+
+    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+
+    // MARK: - UIDocumentPickerDelegate
+
+    public func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        guard let url = urls.first else { return }
+
+        // Check if the selected file's type is allowed
+        let fileUTI = (try? url.resourceValues(forKeys: [.typeIdentifierKey]))?.typeIdentifier ?? ""
+
+        if allowedDocumentTypes.contains(fileUTI) {
+            delegate?.didPickDocument(url)
+        } else {
+            delegate?.didSelectInvalidDocument()
+        }
+    }
+
+    public func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+        delegate?.didCancelDocumentPicker()
+    }
+
+    // MARK: - DocumentType
+
+    public enum DocumentType {
+        case pdf
+        case text
+        case word
+        case all
+
+        var fileTypes: [String] {
+            switch self {
+            case .pdf:
+                return [UTType.pdf.identifier]
+            case .text:
+                return [UTType.text.identifier]
+            case .word:
+                return [
+                    "com.microsoft.word.doc",
+                    "org.openxmlformats.wordprocessingml.document"
+                ]
+            case .all:
+                return [
+                    UTType.pdf.identifier,
+                    UTType.text.identifier,
+                    "com.microsoft.word.doc",
+                    "org.openxmlformats.wordprocessingml.document"
+                ]
+            }
+        }
+    }
+}
---
+++ FilePickerPod/FilePickerPod/FilePickerPod.h
@@ -0,0 +1,18 @@
+//
+//  FilePickerPod.h
+//  FilePickerPod
+//
+//  Created by Aniket Patil on 12/08/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FilePickerPod.
+FOUNDATION_EXPORT double FilePickerPodVersionNumber;
+
+//! Project version string for FilePickerPod.
+FOUNDATION_EXPORT const unsigned char FilePickerPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FilePickerPod/PublicHeader.h>
+
+
