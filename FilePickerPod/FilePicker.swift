de1dc5be47aa3d063a1d2266ec2e12aeaadcf374
Aniket Patil <aniket.p@reapmind.com>
2024-08-13 11:45:26 +0530

    Add file size limit

--- FilePickerPod/FilePicker.swift
+++ FilePickerPod/FilePicker.swift
@@ -6,6 +6,7 @@ public protocol FilePickerDelegate: AnyObject {
     func didPickDocument(_ url: URL)
     func didCancelDocumentPicker()
     func didSelectInvalidDocument()
+    func didFailWithError(_ error: Error)
 }
 
 public class FilePicker: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIDocumentPickerDelegate {
@@ -13,10 +14,22 @@ public class FilePicker: NSObject, UIImagePickerControllerDelegate, UINavigation
     private weak var viewController: UIViewController?
     private weak var delegate: FilePickerDelegate?
     private var allowedDocumentTypes: Set<String> = []
+    
+    // Properties for file size limits
+    public var imageSizeMinLimit: Int
+    public var imageSizeMaxLimit: Int
+    public var documentSizeMinLimit: Int
+    public var documentSizeMaxLimit: Int
 
-    public init(viewController: UIViewController, delegate: FilePickerDelegate) {
+    public init(viewController: UIViewController, delegate: FilePickerDelegate,
+                imageSizeMinLimit: Int, imageSizeMaxLimit: Int,
+                documentSizeMinLimit: Int, documentSizeMaxLimit: Int) {
         self.viewController = viewController
         self.delegate = delegate
+        self.imageSizeMinLimit = imageSizeMinLimit
+        self.imageSizeMaxLimit = imageSizeMaxLimit
+        self.documentSizeMinLimit = documentSizeMinLimit
+        self.documentSizeMaxLimit = documentSizeMaxLimit
     }
 
     public func pickImage() {
@@ -30,7 +43,6 @@ public class FilePicker: NSObject, UIImagePickerControllerDelegate, UINavigation
     public func pickDocument(type: DocumentType) {
         allowedDocumentTypes = Set(type.fileTypes)
         
-        // Update the initializer to use the new method
         let documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: type.contentTypes, asCopy: true)
         documentPicker.delegate = self
         viewController?.present(documentPicker, animated: true, completion: nil)
@@ -40,8 +52,9 @@ public class FilePicker: NSObject, UIImagePickerControllerDelegate, UINavigation
 
     public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[.originalImage] as? UIImage {
-            // Save the image to a temporary file and get the URL
-            if let imageData = image.pngData() {
+            // Check image size limits
+            if let imageData = image.pngData(), imageData.count >= imageSizeMinLimit && imageData.count <= imageSizeMaxLimit {
+                // Save the image to a temporary file and get the URL
                 let tempDirectory = FileManager.default.temporaryDirectory
                 let tempFileURL = tempDirectory.appendingPathComponent(UUID().uuidString).appendingPathExtension("png")
                 do {
@@ -51,6 +64,9 @@ public class FilePicker: NSObject, UIImagePickerControllerDelegate, UINavigation
                     print("Error saving image to temporary file: \(error)")
                     delegate?.didPickImage(image, url: nil)
                 }
+            } else {
+                let error = NSError(domain: "FilePickerError", code: 1, userInfo: [NSLocalizedDescriptionKey: "Image size is out of range."])
+                delegate?.didFailWithError(error)
             }
         }
         picker.dismiss(animated: true, completion: nil)
@@ -65,13 +81,23 @@ public class FilePicker: NSObject, UIImagePickerControllerDelegate, UINavigation
     public func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
         guard let url = urls.first else { return }
 
-        // Check if the selected file's type is allowed
-        let fileUTI = (try? url.resourceValues(forKeys: [.typeIdentifierKey]))?.typeIdentifier ?? ""
+        do {
+            let resourceValues = try url.resourceValues(forKeys: [.fileSizeKey])
+            if let fileSize = resourceValues.fileSize, fileSize >= documentSizeMinLimit && fileSize <= documentSizeMaxLimit {
+                // Check if the selected file's type is allowed
+                let fileUTI = (try? url.resourceValues(forKeys: [.typeIdentifierKey]))?.typeIdentifier ?? ""
 
-        if allowedDocumentTypes.contains(fileUTI) {
-            delegate?.didPickDocument(url)
-        } else {
-            delegate?.didSelectInvalidDocument()
+                if allowedDocumentTypes.contains(fileUTI) {
+                    delegate?.didPickDocument(url)
+                } else {
+                    delegate?.didSelectInvalidDocument()
+                }
+            } else {
+                let error = NSError(domain: "FilePickerError", code: 2, userInfo: [NSLocalizedDescriptionKey: "Document size is out of range."])
+                delegate?.didFailWithError(error)
+            }
+        } catch {
+            delegate?.didFailWithError(error)
         }
     }
 
--- README.md
+++ README.md
@@ -1,2 +1,105 @@
 # FilePickerPod
-                   FilePickerPod is a custom CocoaPods library that provides an easy way to select images or files from the user's device.
+
+FilePicker is a Swift class for picking images and documents in an iOS application, with support for customizable file size limits. It integrates with UIImagePickerController for images and UIDocumentPickerViewController for documents.
+
+
+ [![Version](https://img.shields.io/cocoapods/v/FilePickerPod.svg?style=flat)](https://cocoapods.org/pods/FilePickerPod)
+ [![License](https://img.shields.io/cocoapods/l/FilePickerPod.svg?style=flat)](https://cocoapods.org/pods/FilePickerPod)
+ [![Platform](https://img.shields.io/cocoapods/p/FilePickerPod.svg?style=flat)](https://cocoapods.org/pods/FilePickerPod)
+
+## Requirements
+
+- iOS 15.0+
+- Swift 5.0+
+
+## Installation
+
+### CocoaPods
+
+FilePicker is available through [CocoaPods](https://cocoapods.org/pods/FilePickerPod). To install it, simply add the following line to your Podfile:
+
+```ruby
+pod 'FilePickerPod'
+```
+
+
+## Usage
+
+Initialize FilePicker with required parameters including view controller, delegate, and file size limits.
+
+1. Import the module:
+
+```swift
+import 'FilePickerPod'
+```
+
+2. Initialize FilePicker with required parameters including view controller, delegate, and file size limits.
+
+
+```swift
+import UIKit
+
+class ViewController: UIViewController, FilePickerDelegate {
+
+    private var filePicker: FilePicker?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Initialize FilePicker with size limits
+        filePicker = FilePicker(
+            viewController: self,
+            delegate: self,
+            imageSizeMinLimit: 100 * 1024, // 100 KB
+            imageSizeMaxLimit: 5 * 1024 * 1024, // 5 MB
+            documentSizeMinLimit: 200 * 1024, // 200 KB
+            documentSizeMaxLimit: 10 * 1024 * 1024 // 10 MB
+        )
+    }
+
+    @IBAction func pickImageButtonTapped(_ sender: UIButton) {
+        filePicker?.pickImage()
+    }
+
+    @IBAction func pickDocumentButtonTapped(_ sender: UIButton) {
+        filePicker?.pickDocument(type: .all) // You can change to .pdf, .text, or .word
+    }
+
+    // MARK: - FilePickerDelegate
+
+    func didPickImage(_ image: UIImage, url: URL?) {
+        // Handle picked image
+        print("Image picked. URL: \(url?.absoluteString ?? "No URL")")
+    }
+
+    func didPickDocument(_ url: URL) {
+        // Handle picked document
+        print("Document picked. URL: \(url.absoluteString)")
+    }
+
+    func didCancelDocumentPicker() {
+        // Handle cancellation
+        print("Document picker was cancelled")
+    }
+
+    func didSelectInvalidDocument() {
+        // Handle invalid document type
+        print("Selected document is invalid")
+    }
+
+    func didFailWithError(_ error: Error) {
+        // Handle error
+        print("Error: \(error.localizedDescription)")
+    }
+}
+
+
+
+```
+
+
+
+
+
+
+
